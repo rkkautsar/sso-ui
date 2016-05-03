6224de815fdb5b7c5b03c2843c07c12c1d8af573
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2016-05-03 20:33:13 +0700

    Last commit breaks functionality; - Refactor using global middleware - Remove injected next() function - Stores user in req.sso_user (default) - TODO: Update README - TODO: Bump version

--- index.js
+++ index.js
@@ -8,43 +8,38 @@ function SSO(options) {
         cas_url: "https://sso.ui.ac.id/cas2",
         service_url: options.url,
         cas_version: '2.0',
-        session_name: options.session_sso,
-        session_info: options.session_sso + '_info',
+        session_name: session_sso,
+        session_info: session_sso + '_info',
     });
 
-    this.login = function(req, res, next) {
-
-        var sso = this;
-
-        var inject_next = function() {
-           sso.user = {
-                username: req.session[options.session_sso],
-                fullname: req.session[options.session_sso + '_info'].nama,
-                role: req.session[options.session_sso + '_info'].peran_user,
-                npm: req.session[options.session_sso + '_info'].npm,
-                org_code: req.session[options.session_sso + '_info'].kd_org,
-                org: orgCodeDetails[req.session[options.session_sso + '_info'].kd_org]
+    this.middleware = function(req, res, next) {
+        if (req.session[session_sso]) {
+            req[session_sso] = {
+                username: req.session[session_sso],
+                fullname: req.session[session_sso + '_info'].nama,
+                role: req.session[session_sso + '_info'].peran_user,
+                npm: req.session[session_sso + '_info'].npm,
+                org_code: req.session[session_sso + '_info'].kd_org,
+                org: orgCodeDetails[req.session[session_sso + '_info'].kd_org]
             };
+        } else {
+            req[session_sso] = null;
+        }
 
-            next();
-        };
-
-        this.cas.bounce(req, res, inject_next);
-    };
-
-    this.block = function(req, res, next) {
-        this.cas.block(req, res, next);
-    };
+        next();
+    }
 
     this.clear = function(req, res, next) {
-        req.session.destroy();
-        next();
+       req.session.destroy();
+       this.user = null;
+       next();
     };
 
+    this.login = this.cas.bounce;
+    this.block = this.cas.block;
     this.logout = this.cas.logout;
 
-    this.login = this.login.bind(this);
-    this.block = this.block.bind(this);
+    this.clear = this.clear.bind(this);
 }
 
 module.exports = SSO;
